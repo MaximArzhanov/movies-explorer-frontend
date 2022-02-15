c796e9ffa2409403f3ff2a45fd67bc8fcd13b16e
Maxim Arzhanov <Arzhanov93@yandex.ru>
2022-02-15 15:29:39 +0300

    feat: Создан компонент AboutProject (JSX и Вёрстка)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
 import Footer from '../Footer/Footer';
+import AboutProject from '../AboutProject/AboutProject';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Promo />
+      <AboutProject />
       <Footer />
     </div>
   );
