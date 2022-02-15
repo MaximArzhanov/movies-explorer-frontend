1294b5a2880fe94a1739c9a9f9f15455e7903b0b
Maxim Arzhanov <Arzhanov93@yandex.ru>
2022-02-15 10:24:57 +0300

    feat: Создан компонент Footer (Вёрстка и JSX)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,9 +6,13 @@ function Footer() {
 
   return (
     <footer className="footer">
-      <Link to="/" className="footer__link">Яндекс.Практикум</Link>
-      <Link to="/" className="footer__link">Github</Link>
-      <Link to="/" className="footer__link">Facebook</Link>
+      <p className="header__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <span className="header__copyright">&copy; 2022</span>
+      <ul className="footer__links">
+        <Link to="/" className="footer__link">Яндекс.Практикум</Link>
+        <Link to="/" className="footer__link">Github</Link>
+        <Link to="/" className="footer__link">Facebook</Link>
+      </ul>
     </footer>
   );
 }
