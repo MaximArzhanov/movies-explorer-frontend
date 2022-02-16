843be1c5562270bee9e0de5c5ecae4c0eaa81bae
Maxim Arzhanov <Arzhanov93@yandex.ru>
2022-02-16 11:22:51 +0300

    feat: Создан компонент Main (JSX и Вёрстка)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
-import Promo from '../Promo/Promo';
+import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
-import AboutProject from '../AboutProject/AboutProject';
-import Techs from '../Techs/Techs';
-import AboutMe from '../AboutMe/AboutMe';
-import Portfolio from '../Portfolio/Portfolio';
+
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Promo />
-      <AboutProject />
-      <Techs />
-      <AboutMe />
-      <Portfolio />
+        <Main />
       <Footer />
     </div>
   );
