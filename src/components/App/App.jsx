5d449f4e1be4285dc22603e150c6f67b5399d182
Maxim Arzhanov <Arzhanov93@yandex.ru>
2022-02-16 12:40:37 +0300

    feat: Создан компонент MoviesCard (JSX и Вёрстка)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,13 @@ import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
-
+import MoviesCard from '../MoviesCard/MoviesCard';
 
 function App() {
   return (
     <div className="app">
       <Header />
-        <Main />
+        <MoviesCard />
       <Footer />
     </div>
   );
---
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './MoviesCard.css';
+import moviePicture from '../../images/movie-picture.png'
+
+function MoviesCard() {
+
+  return (
+    <article className="movies-card">
+        <figure className="movie">
+            <figcaption className="movie__title">
+                В погоне за Бенкси
+            </figcaption>
+            <span className="movie__duration">27 минут</span>
+            <img src={moviePicture} alt="Постер к фильму" className="movie__picture" />
+        </figure>
+        <button className="movies-card__button">Сохранить</button>
+    </article>
+  );
+}
+
+export default MoviesCard;
