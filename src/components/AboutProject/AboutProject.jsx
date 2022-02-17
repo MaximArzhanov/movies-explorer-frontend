1c5fce87bedfe4deae2ffd6a2b663e800ecb035b
Maxim Arzhanov <Arzhanov93@yandex.ru>
2022-02-17 21:52:07 +0300

    fix: Исправлены стили для компонента AboutProject

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import './AboutProject.css';
+import '../styles.css';
 
 function AboutProject() {
 
   return (
     <section className="about-project">
-        <h2 className="about-project__title">О проекте</h2>
+        <h2 className="section-title">О проекте</h2>
+        <div className="about-project__text-content">
+
+            <aside className="about-project__container">
+                <h3 className="about-project__subtitle">
+                    Дипломный проект включал 5 этапов
+                </h3>
+                <p className="about-project__paragraph">
+                    Составление плана, работу над бэкендом, вёрстку,
+                    добавление функциональности и финальные доработки.
+                </p>
+            </aside>
+
+            <aside className="about-project__container">
+                <h3 className="about-project__subtitle">
+                    На выполнение диплома ушло 5 недель
+                </h3>
+                <p className="about-project__paragraph">
+                    У каждого этапа был мягкий и жёсткий дедлайн,
+                    которые нужно было соблюдать, чтобы успешно защититься.
+                </p>
+            </aside>
+
+        </div>
+
+        <div className="about-project__time-bar">
+            <p className="about-project__time-line 
+                        about-project__time-line_color_green">
+                1 неделя
+            </p>
+            <p className="about-project__time-line">4 недели</p>
+            <span className="about-project__skill">Back-end</span>
+            <span className="about-project__skill">Front-end</span>
+        </div>
+        
+    {/* <h2 className="section-title">О проекте</h2>
         <div className="about-project__container">
             <h3 className="about-project__subtitle">
                 Дипломный проект включал 5 этапов
@@ -36,7 +72,7 @@ function AboutProject() {
                 <p className="about-project__time-line">4 недели</p>
                 <span className="about-project__skill">Front-end</span>
             </div>
-        </div>
+        </div> */}
     </section>
   );
 }
